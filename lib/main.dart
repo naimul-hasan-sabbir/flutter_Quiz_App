85d29e549f49ccd2da937d23fdcf02db974dd33c
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-02-06 23:54:18 +0600

    added files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello!'),)
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
   }
 }
