7bf071313ef89e080e7f365ced96bff0109edd90
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-02-07 23:03:12 +0600

    Experiment

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first app"),
+        ),
+        body: Text("This is the body of my app"),
+      ),
     );
   }
 }
