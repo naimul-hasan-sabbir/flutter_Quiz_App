175a6baa3651ebb61b6c6e52637c6ae3a4b3a14b
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-02-27 23:56:00 +0600

    updated

--- lib/question.dart
+++ lib/question.dart
@@ -10,9 +10,14 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      questionText,
-      style: TextStyle(fontSize: 28),
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
