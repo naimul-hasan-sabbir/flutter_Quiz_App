843619ad90cb7b0a99faf4b5533335241f346dc1
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-03-03 23:58:18 +0600

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,12 +37,9 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(questions.elementAt(_questionIndex)),
-            ElevatedButton(
-              child: Text("Answer 1"),
-              onPressed: _answerQuestion,
-            ),
-            ElevatedButton(child: Text("Answer 2"), onPressed: _answerQuestion),
-            ElevatedButton(child: Text("Answer 3"), onPressed: _answerQuestion),
+            Answer(),
+            Answer(),
+            Answer(),
           ],
         ),
       ),
