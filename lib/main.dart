1297dc3eb3c915437f13573f98b0b5974e6b475f
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-03-07 23:49:36 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -45,9 +45,10 @@ class _MyAppState extends State<MyApp> {
             Question(
               questions[_questionIndex]['questionText'] as String,
             ),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
