6b19daa96304aaca32f1485c1c6f9e9e376f7270
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-03-23 22:53:31 +0600

    score added

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 8) {
+      resultText = "You are awesome & innocent";
+    } else if (resultScore <= 12) {
+      resultText = "You are good one lad!";
+    } else {
+      resultText = "You are evil";
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("You Did It!!!"));
+    return Center(
+        child: Text(
+      resultPhrase,
+      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
+    ));
   }
 }
